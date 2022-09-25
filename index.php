722cb221da61c47680ee526be8a3290c92e7834c
botzen67 <tony@audifamily.net>
2022-09-25 05:35:40 +0100

    Added meta tag for better viewing on mobile

--- index.php
+++ index.php
@@ -68,6 +68,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <meta http-equiv="refresh" content="10">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 <title>Sauna Controller</title>
 <style rel="stylesheet" type="text/css">
 
