ab357203ce9e4bfd7073b20b59da2d69da998fa5
botzen67 <tony@audifamily.net>
2024-01-31 00:18:42 -0500

    added temp data plotter

---
+++ graph_temp_data.py
@@ -0,0 +1,25 @@
+import urllib.request as url
+from dateutil import parser
+import datetime
+import matplotlib.pyplot as plt
+import pandas as pd
+
+datafile = url.urlopen("http://hockeytownsauna.botzen.com/temp_record.dat")
+data = []
+for line in datafile:
+    datapoint = line.strip().decode('utf-8')
+    fields = datapoint.split("   ")
+    timestamp = parser.parse(fields[0])
+    temp = int(fields[1])
+    onoff = int(fields[2])
+    if temp > 1 and timestamp > datetime.datetime(2024,1,28,10):
+        data.append((timestamp,temp))
+        # data.append(temp)
+        # print(timestamp, temp, onoff)
+# print(len(data))
+df =  pd.DataFrame(data, columns = ['Date', 'Temp'])
+# print(df)
+plt.plot(df['Date'],df['Temp'])
+plt.show()
+
+    
