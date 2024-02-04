cde3a4980df120c524dcb2cb077ebe3d44b38e19
botzen67 <botzen67@gmail.com>
2024-02-04 10:09:58 -0500

    fixed algo

--- sound_notify.py
+++ sound_notify.py
@@ -10,17 +10,16 @@ while(True):
         set_point = int(file.readline())
     with open("temp.dat") as file:
         current_temp = int(file.readline())
-    trip_point = set_point - 7
-    print (on_off, heater_on, set_point, current_temp)
+    # print (on_off, heater_on, set_point, current_temp)
     
-    if (not(tripped) and heater_on and current_temp > trip_point):
-        print("Notifying!")    
+    if (not(tripped) and heater_on and current_temp > set_point - 7):
+        # print("Notifying!")    
         # play_sound_command = "aplay bleep_01.wav"
         play_sound_command = "./sound_notify.sh"
         os.system(play_sound_command)
         tripped = True
     
-    if (current_temp < trip_point):
+    if (tripped and current_temp < set_point and not(on_off)):
         tripped = False
 
     time.sleep(10)
